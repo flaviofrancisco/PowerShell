c3c086144384700fe1d47eb8694aff4a65a26dd6
flaviofrancisco <flaviofrancis@msn.com>
2019-08-28 10:09:11 +0200

    Update database-sql-server-select-from-table.ps1

--- database-sql-server-select-from-table.ps1
+++ database-sql-server-select-from-table.ps1
@@ -2,7 +2,7 @@ $instance = 'Your instance'
 
 $database_name = 'Your database name'
 
-$serverInstance = New-Object ('Microsoft.SqlServer.Management.Smo.Server') "{$database_name}"
+$serverInstance = New-Object ('Microsoft.SqlServer.Management.Smo.Server') "{$instance}"
 
 $db = $serverInstance.Databases['{$database_name}']
 
